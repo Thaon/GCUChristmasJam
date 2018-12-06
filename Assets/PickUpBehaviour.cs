7ac8565dda0a6ecd0567fda3bb45755c26bd34f3
jokehboy <jokehboy@gmail.com>
2018-12-06 19:31:25 +0000

    Ben
    Added draggy drop

---
+++ Assets/PickUpBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpBehaviour : StateMachineBehaviour {
+
+    private bool _mouseState;
+    private GameObject target;
+    public Vector3 screenSpace;
+    public Vector3 offset;
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        target = animator.GetComponent<GameObject>();
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hitInfo;
+            target = GetClickedObject(out hitInfo);
+            if (target != null)
+            {
+                _mouseState = true;
+                screenSpace = Camera.main.WorldToScreenPoint(target.transform.position);
+                offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _mouseState = false;
+        }
+        if (_mouseState)
+        {
+            //keep track of the mouse position
+            var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+
+            //convert the screen mouse position to world point and adjust with offset
+            var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+
+            //update the position of the object in the world
+            target.transform.position = curPosition;
+        }
+    }
+
+
+    GameObject GetClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            target = hit.collider.gameObject;
+        }
+
+        return target;
+    }
+}
+
+	
--- Assets/RoamingBehaviour.cs
+++ Assets/RoamingBehaviour.cs
@@ -36,6 +36,9 @@ public class RoamingBehaviour : StateMachineBehaviour {
             timer = 0;
         }
 
+        
+
+
 	}
     
 
