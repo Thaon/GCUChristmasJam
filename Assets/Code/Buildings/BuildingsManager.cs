18ccbc3b0e13c6be37a886d6b8becdbc0f7e1616
YAP! <GMADDA200@caledonian.ac.uk>
2018-12-06 19:06:59 +0000

    finished building system

--- Assets/Code/Buildings/BuildingsManager.cs
+++ Assets/Code/Buildings/BuildingsManager.cs
@@ -8,6 +8,7 @@ public class BuildingsManager : MonoBehaviour {
 
     public BuildingsPrefab[] m_buildingsPrefabs;
     public int m_selectedBuilding = -1;
+    public Material m_canPlace, m_cannotPlace;
 
     private GameObject m_activeSelection = null;
     private PersistentData m_pData;
@@ -27,8 +28,25 @@ public class BuildingsManager : MonoBehaviour {
             SelectBuilding(0);
         }
 
+        //change material
+        if (m_activeSelection != null)
+        {
+            Collider[] buildingsInArea = Physics.OverlapSphere(m_activeSelection.transform.position, 10);
+
+            foreach (Collider coll in buildingsInArea)
+            {
+                if (coll.tag == "Building")
+                {
+                    m_activeSelection.GetComponentInChildren<MeshRenderer>().sharedMaterial = m_cannotPlace;
+                    return;
+                }
+            }
+            m_activeSelection.GetComponentInChildren<MeshRenderer>().sharedMaterial = m_canPlace;
+
+        }
+
         //spawn building
-        if (Input.GetMouseButtonDown(0))
+        if (m_activeSelection != null && Input.GetMouseButtonDown(0))
         {
             //check collisions
             Vector3 pos1 = m_activeSelection.transform.position;
@@ -36,8 +54,11 @@ public class BuildingsManager : MonoBehaviour {
 
             Collider[] buildingsInArea = Physics.OverlapSphere(m_activeSelection.transform.position, 10);
 
-            if (buildingsInArea.Length != 0)
-                return;
+            foreach (Collider coll in buildingsInArea)
+            {
+                if (coll.tag == "Building")
+                    return;
+            }
 
             //spawn if have resources
             if (m_pData.m_presentsProduced >= m_buildingsPrefabs[m_selectedBuilding].m_price)
