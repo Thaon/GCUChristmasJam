82a4d38f347b4736c5b48d43ef1ca5316bf6bfd3
jokehboy <jokehboy@gmail.com>
2018-12-06 19:06:35 +0000

    Ben
    AI n that

---
+++ Assets/RoamingBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamingBehaviour : StateMachineBehaviour {
+
+    public float wanderRadius;
+    public float wanderTimer;
+
+  
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+
+
+	}
+
+	//OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        timer += Time.deltaTime;
+        if(timer>=wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(animator.transform.position, wanderRadius, -1);
+            newPos.y = 0;
+            agent.SetDestination(newPos);
+            agent.isStopped = false;
+            timer = 0;
+        }
+
+	}
+    
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDir = Random.insideUnitSphere * dist;
+        randDir += origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDir, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
